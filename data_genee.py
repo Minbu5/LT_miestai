1b344d6faa34736f71d52b0c86be967aa7c1986a
Minbu5 <128700993+Minbu5@users.noreply.github.com>
2024-08-02 10:15:32 +0300

    Add files via upload
    data_genee.py recification. Update data table index in pandas

--- data_genee.py
+++ data_genee.py
@@ -10,9 +10,9 @@ def d_genee():
     # __________________103_miestai.csv generavimas____________________________________
     miestu_duomenys = pd.read_html(WEB_SALTINIS)
 
-    m_sarasas = miestu_duomenys[0]["Miestas"]
+    m_sarasas = miestu_duomenys[1]["Miestas"]
     kord_duomenys = pd.read_csv(KORD_SALTINIS, index_col=[0])
-    gyv_sk = miestu_duomenys[0]["Gyventojų skaičius (2019 m.)"]
+    gyv_sk = miestu_duomenys[1]["Gyventojų skaičius (2019 m.)"]
 
     bendras = pd.concat([m_sarasas, kord_duomenys, gyv_sk], axis=1)
 
@@ -23,3 +23,4 @@ def d_genee():
     rusiuotas.to_csv("103_miestai.csv")
     rusiuotas.head(MAZIAUSIU_TOP_50).to_csv("50_maziausiu_pagal_gyv.csv")
     rusiuotas.head(MAZIAUSIU_TOP_20).to_csv("20_maziausiu_pagal_gyv.csv")
+d_genee()
